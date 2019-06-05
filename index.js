9c073ac1a6a1d22ef8008b13fb6309e9eac353e0
ikarashi <jhypd380@yahoo.co.jp>
2019-06-05 23:03:30 +0900

    Update index.js
    formmat & replace fs.exists→fs.stat

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const commandLineArgs = require('command-line-args');
+const commandLineArgs = require("command-line-args");
 const PDFDocument = require("pdfkit");
-const fs = require("fs")
-const imageSize = require("image-size")
-const path = require("path")
+const fs = require("fs");
+const imageSize = require("image-size");
+const path = require("path");
 
-const optionDef = [{
+const optionDef = [
+  {
     name: "dir",
     alias: "d",
     type: String
@@ -14,59 +15,63 @@ const optionDef = [{
     alias: "o",
     type: String
   }
-]
+];
 
 const options = commandLineArgs(optionDef);
 
-fs.exists(options.dir, (isExist) => {
-
-  if (!isExist) return;
+fs.stat(options.dir, err => {
+  if (err) return;
 
   fs.readdir(options.dir, (err, files) => {
     if (err) throw err;
 
-    const list = files.filter((fileName) => {
-      return fs.statSync(options.dir + "/" + fileName).isFile() && /.*\.jpg$/.test(fileName);
-    }).map((fileName) => {
+    const list = files
+      .filter(fileName => {
+        return (
+          fs.statSync(options.dir + "/" + fileName).isFile() &&
+          /.*\.jpg$/.test(fileName)
+        );
+      })
+      .map(fileName => {
+        const padding = fileName.replace(".jpg", "");
+        return {
+          origin: fileName,
+          padding: Number(padding)
+        };
+      });
 
-      const padding = fileName.replace(".jpg", "");
-      return {
-        origin: fileName,
-        padding: Number(padding)
-      }
-    })
-
-    const outputFile = selectOutputFile(options.output, options.dir)
+    const outputFile = selectOutputFile(options.output, options.dir);
 
     const doc = new PDFDocument({
       autoFirstPage: false
     });
 
     doc.pipe(fs.createWriteStream(outputFile));
-    const result = list.sort((a, b) => a.padding - b.padding)
-    result.map(fileObject => {
-      return options.dir.endsWith("/") ? options.dir + fileObject.origin : options.dir + "/" + fileObject.origin;
-    }).forEach(filePath => {
-      const dimensions = imageSize(filePath)
-
-      doc.addPage({
-        size: [dimensions.width, dimensions.height]
+    const result = list.sort((a, b) => a.padding - b.padding);
+    result
+      .map(fileObject => {
+        return options.dir.endsWith("/")
+          ? options.dir + fileObject.origin
+          : options.dir + "/" + fileObject.origin;
       })
+      .forEach(filePath => {
+        const dimensions = imageSize(filePath);
 
-      doc.image(filePath, 0, 0, {
-        width: dimensions.width
-      })
-    })
+        doc.addPage({
+          size: [dimensions.width, dimensions.height]
+        });
+
+        doc.image(filePath, 0, 0, {
+          width: dimensions.width
+        });
+      });
     doc.end();
-
-  })
-
-})
+  });
+});
 
 function selectOutputFile(filePath, dir) {
-
   if (!filePath) {
-    return path.basename(dir) + ".pdf"
+    return path.basename(dir) + ".pdf";
   }
 
   return !filePath.endsWith(".pdf") ? filePath + ".pdf" : filePath;
